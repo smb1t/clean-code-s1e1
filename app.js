680e6bea095b36ac1cff579a32eb140bd9d3f6d1
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-05-30 15:36:17 +0500

    refactor: add class names and ids according to rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementById("add-btn");//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -32,18 +32,21 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.classList = "list-item field";
+
     label.innerText=taskString;
-    label.className='task';
+    label.className = "field-label";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="field-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="field-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn btn-text js-btn-edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn btn-icon js-btn-delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,12 +87,12 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var editBtn=listItem.querySelector(".js-btn-edit");
+    var containsClass=listItem.classList.contains("open-for-edit");
+    //If class of the parent is .open-for-edit
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .open-for-edit
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +101,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .open-for-edit on the parent.
+    listItem.classList.toggle("open-for-edit");
 };
 
 
@@ -156,8 +159,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".js-btn-edit");
+    var deleteButton=taskListItem.querySelector(".js-btn-delete");
 
 
     //Bind editTask to edit button.
